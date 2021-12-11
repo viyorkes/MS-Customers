4288ca198618ed00a0a0ed31ac4afbcfd885b18f
victor <victorsleiman25@gmail.com>
2021-12-10 21:23:00 -0300

    creating controller simple test

---
+++ src/test/java/com/customer/controller/CustomerControllerTest.java
@@ -0,0 +1,37 @@
+package com.customer.controller;
+
+import com.customer.entity.Customer;
+import com.customer.repository.CustomerRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CustomerControllerTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    public void retrieveAllCustomers() {
+
+        Customer customer = new Customer();
+        customer.setId(1);
+        customer.setBirthDate(new Date());
+        customer.setName("teste");
+        customer.setSalary(new BigDecimal( 150));
+        
+        Iterable<Customer> customers = customerRepository.findAll();
+        assertThat(customer).isNotNull();
+
+    }
+    }
+

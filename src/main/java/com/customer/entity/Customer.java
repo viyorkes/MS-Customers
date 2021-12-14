f024130b98afae630390f2c180cef3a16b675b0e
victor <victorsleiman25@gmail.com>
2021-12-14 19:16:52 -0300

    creating flow Post from Customers

--- src/main/java/com/customer/entity/Customer.java
+++ src/main/java/com/customer/entity/Customer.java
@@ -8,9 +8,10 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.math.BigDecimal;
 import java.util.Date;
-
+import java.util.List;
 
 
 @Data
@@ -26,5 +27,8 @@ public class Customer {
     private Date birthDate;
     private String departament;
     private BigDecimal salary;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Post> posts;
 
 }
